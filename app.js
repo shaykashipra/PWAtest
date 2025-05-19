2128c2253b0030919a9aadd1190a61328d5ac07e
Shayka Shipra <triviatruism@gmail.com>
2025-05-20 01:37:06 +0600

    added

---
+++ app.js
@@ -0,0 +1,6 @@
+// app.js
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(() => console.log('Service Worker registered!'));
+  }
+  
---
+++ service-worker.js
@@ -0,0 +1,15 @@
+// service-worker.js
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('static').then((cache) => {
+        return cache.addAll(['./', './index.html', './app.js']);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+  });
+  
