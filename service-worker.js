086fb246aad0ef75a912f89bd4dd1cd6348d6cb4
Shayka Shipra <triviatruism@gmail.com>
2025-05-20 01:53:32 +0600

    updated

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,26 @@
-// service-worker.js
+const CACHE_NAME = 'static-v1';
+
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('static').then((cache) => {
-        return cache.addAll(['./', './index.html', './app.js']);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((res) => res || fetch(e.request))
-    );
-  });
-  
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./', './index.html', './app.js']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
